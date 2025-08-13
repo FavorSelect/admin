55c7573f4d86a493b3de7516caba429801933308
Satish Singh <i.sksingh113@gmail.com>
2025-08-13 23:40:02 +0530

    some check

--- src/app/banner/page.tsx
+++ src/app/banner/page.tsx
@@ -3,4 +3,5 @@ import BannerWrapper from "@/components/organisms/banner/BannerWrapper";
 export default async function Banner() {
   return <BannerWrapper />;
 }
-//some check
+//some check are falied in this file
+// now i am trying check webhook server by commiting this file
